c510710581fdd4e4ea01b7c3468c0f2de31d4c98
Kimbrian Marshall <kimbrian.marshall@brik.id>
2024-03-27 00:26:40 +0700

    should have watched through the episode

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 let count = 1;
+const choices = [
+  [4, 1],
+  [0, 5],
+  //looking at the position Will clicked, probably a 3
+  [2, 4],
+  [1, 3],
+  [5, 0],
+];
 
 const counter = document.getElementById("counter");
-renderCount();
+const yes = document.getElementById("yes");
+const no = document.getElementById("no");
+render();
 
 const buttons = document.getElementsByClassName("button");
-const no = buttons[1];
-const yes = buttons[4];
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", () => {
+    const choice = choices[count - 1];
+    if (choice[0] == i) {
+      count++;
+      if (count <= 5) return render();
+      window.location.href = "https://www.netflix.com/watch/81450505?t=2324";
+    }
+    if (choice[1] == i) {
+      location.reload();
+    }
+  });
+}
 
-no.addEventListener("click", () => {
-  location.reload();
-});
-
-yes.addEventListener("click", () => {
-  count++;
-  if (count <= 5) return renderCount();
-  window.location.href = "https://www.netflix.com/watch/81450505?t=2324";
-});
-
-function renderCount() {
+function render() {
   counter.innerHTML = `${count} of 5`;
+  yes.innerHTML = choices[count - 1][0] + 1;
+  no.innerHTML = choices[count - 1][1] + 1;
 }
